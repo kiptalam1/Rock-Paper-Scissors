c5399e893255e6c84fa7a9e1d14b336f657cdd1f
adams <adamskiptalam0@gmail.com>
2024-10-18 20:26:22 +0300

    Increase the number of rounds to five using playGame function.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -9,28 +6,48 @@ function getComputerChoice() {
     return choice;
 }
 
-// console.log(getComputerChoice());
 
 function getHumanChoice() {
     let choice = prompt("Enter your choice (rock/paper/scissors): ");
     return choice.toLowerCase();
 }
     
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-// // console.log(getHumanChoice());
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
-        humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}.`;
-    } else if (humanChoice === computerChoice) {
-        return "Its a draw!";
+    function playRound(humanChoice, computerChoice) {
+        if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}.`;
+        } else if (humanChoice === computerChoice) {
+            return "Its a draw!";
+        } else {
+            computerScore++;
+            return `You lose! ${computerChoice} beats ${humanChoice}.`;
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        if (humanSelection === 'rock' || humanSelection === "paper" || humanSelection === "scissors") {
+            console.log(playRound(humanSelection, computerSelection));
+            console.log(`Scores: Human ${humanScore} - Computer ${computerScore}`);
+        } else {
+            console.log("Invalid choice! Please enter 'rock', 'paper', or 'scissors'.");
+            i--;
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`Final Result: You win the game! Human ${humanScore} - Computer ${computerScore}`);
+    } else if (computerScore > humanScore) {
+        console.log(`Final Result: You lose the game! Human ${humanScore} - Computer ${computerScore}`);
     } else {
-        computerScore++;
-        return `You lose! ${computerChoice} beats ${humanChoice}.`;
+        console.log(`Final Result: It's a draw! Human ${humanScore} - Computer ${computerScore}`);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
+playGame();
