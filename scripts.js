47111105f2b25043c74e0cbd5cd222a39fae05d4
adams <adamskiptalam0@gmail.com>
2024-10-18 19:23:22 +0300

    Create functions for humanChoice and computerChoice

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let random = Math.floor(Math.random() * 3);
+    let choice = choices[random];
+    return choice;
+}
+
+// console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("Enter a number 1/2/3: ");
+    choice = parseInt(choice);
+    if (choice === 1) {
+        return 'rock';
+    } else if (choice === 2) {
+        return 'paper';
+    } else if (choice === 3) {
+        return 'scissors';
+    } else {
+        return "Enter a valid choice!";
+    }
+}
+
+// console.log(getHumanChoice());
