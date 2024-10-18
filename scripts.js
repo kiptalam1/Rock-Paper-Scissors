82432cc1e6e52d496c4eaf351ef3876a88b24145
adams <adamskiptalam0@gmail.com>
2024-10-18 20:01:47 +0300

    Create playRound function which give the game its logic.

--- scripts.js
+++ scripts.js
@@ -12,17 +12,25 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let choice = prompt("Enter a number 1/2/3: ");
-    choice = parseInt(choice);
-    if (choice === 1) {
-        return 'rock';
-    } else if (choice === 2) {
-        return 'paper';
-    } else if (choice === 3) {
-        return 'scissors';
+    let choice = prompt("Enter your choice (rock/paper/scissors): ");
+    return choice.toLowerCase();
+}
+    
+
+// // console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}.`;
+    } else if (humanChoice === computerChoice) {
+        return "Its a draw!";
     } else {
-        return "Enter a valid choice!";
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}.`;
     }
 }
 
-// console.log(getHumanChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
